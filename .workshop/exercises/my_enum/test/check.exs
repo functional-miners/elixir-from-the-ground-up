285e93d0848b9c9a5ba03c5fc3664e103e653e85
Wojciech Gawronski <afronski@gmail.com>
2016-01-10 13:12:33 +0100

    Partially working detection for cheating in 03 (#4).

--- .workshop/exercises/my_enum/test/check.exs
+++ .workshop/exercises/my_enum/test/check.exs
@@ -1,6 +1,56 @@
 defmodule Workshop.Exercise.MyEnumCheck do
   use Workshop.Validator
 
+  defmodule Tracer do
+    use GenServer
+
+    def start(), do: GenServer.start(__MODULE__, [])
+
+    def trace(who, modules, functions), do: GenServer.call(who, {:trace, modules, functions})
+    def clear(who), do: GenServer.call(who, :clear)
+
+    def calls(who), do: GenServer.call(who, :get_calls)
+
+    def init([]) do
+      :erlang.trace(:all, true, [:call])
+
+      {:ok, %{ :modules => [], :functions => [], :calls => [] }}
+    end
+
+    def handle_call(:get_calls, _from, state) do
+      {:reply, state[:calls], state}
+    end
+
+    def handle_call({:trace, modules, functions}, _from, state) do
+      for module <- modules do
+        for function <- functions do
+          IO.puts "ON: #{module}.#{function}"
+          a = :erlang.trace_pattern({module, function, :_}, [{:_, [], [{:return_trace}]}])
+          IO.puts "ASSIGNED: #{a}"
+        end
+      end
+
+      {:reply, :traced, %{ state | :functions => functions, :modules => modules }}
+    end
+
+    def handle_call(:clear, _from, state) do
+      for module <- state[:modules] do
+        for function <- state[:functions] do
+          IO.puts "OFF: #{module}.#{function}"
+          :erlang.trace_pattern({module, function, :_}, false)
+        end
+      end
+
+      {:reply, :cleared, %{ state | :functions => [], :modules => [], :calls => [] }}
+    end
+
+    def handle_info({:trace, _, :call, {_mod, call, _args}}, state) do
+      {:noreply, %{ state | :calls => state[:calls] ++ [ call ] }}
+    end
+
+    def handle_info(_, state), do: {:noreply, state}
+  end
+
   defmacrop check_the_same(expected, given) do
     quote do
       if unquote(expected) == unquote(given) do
@@ -11,6 +61,31 @@ defmodule Workshop.Exercise.MyEnumCheck do
            "as `#{unquote(Macro.to_string(expected))}` - " <>
            "#{inspect unquote(expected)}, instead got: #{inspect unquote(given)}."}
       end
+    end
+  end
+
+  defmacrop should_not_use(module, function, call) do
+    quote do
+      {:ok, pid} = Tracer.start()
+
+      Tracer.trace(pid, [ unquote(module) ], [ unquote(function) ])
+      unquote(call)
+
+      :timer.sleep(100)
+      calls = Tracer.calls(pid)
+
+      result = if length(calls) > 0 do
+        {:error,
+         "Call to `#{unquote(Macro.to_string(module))}.#{unquote(Macro.to_string(function))}` detected. " <>
+           "You cannot use those function in your `#{unquote(Macro.to_string(call))}` implementation."}
+      else
+        :ok
+      end
+
+      Tracer.clear(pid)
+      Process.exit(pid, :normal)
+
+      result
     end
   end
 
@@ -29,8 +104,29 @@ defmodule Workshop.Exercise.MyEnumCheck do
                    MyEnum.append([1, 2, 3], [4, 5, 6]))
   end
 
-  verify "reverse" do
+  verify "length" do
     check_the_same(length(:lists.seq(1, 10000)),
                    MyEnum.length(:lists.seq(1, 10000)))
+  end
+
+  verify "reverse" do
+    check_the_same(Enum.reverse(:lists.seq(1, 10000)),
+                   MyEnum.reverse(:lists.seq(1, 10000)))
+  end
+
+  verify "do not use :erlang.++/2 function in MyEnum.append/2" do
+    should_not_use(:erlang, :'++', MyEnum.append([1], [2]))
+  end
+
+  verify "do not use Kernel.length/1 function in MyEnum.length/1" do
+    should_not_use(Kernel, :length, MyEnum.length([1, 2, 3]))
+  end
+
+  verify "do not use Enum.map/2 function in MyEnum.map/2" do
+    should_not_use(Enum, :map, MyEnum.map(["a"], &String.upcase/1))
+  end
+
+  verify "do not use Enum.reverse/2 function in MyEnum.reverse/2" do
+    should_not_use(Enum, :reverse, MyEnum.reverse([1, 2, 3]))
   end
 end
